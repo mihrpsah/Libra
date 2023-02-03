e3bc6ba9cb6010e2ef95ab9ef23bfcee9fea99e0
mihrpsah <smihirp1@gmail.com>
2023-02-03 16:32:18 +0530

    lunch

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-document.addEventListener('onload', () =>{
+window.addEventListener('load', () =>{
     
+    console.log("Log 1");
     let books = JSON.parse(localStorage.getItem('books')) || [];
+    const addBook = document.querySelector("[data-addBook]");
     
+    addBook.addEventListener('submit', (e)=>{
+            console.log("Log 2")
+            console.log(e.target.elements.book.value);
+            console.log(e.target.elements.status.value);
+    })
 
 })
