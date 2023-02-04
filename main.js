be50934243fcfae8faabacfa770e8d8c5bb59e00
mihrpsah <smihirp1@gmail.com>
2023-02-04 12:15:33 +0530

    ...

--- main.js
+++ main.js
@@ -8,6 +8,14 @@ window.addEventListener('load', () =>{
             console.log("Log 2")
             console.log(e.target.elements.book.value);
             console.log(e.target.elements.status.value);
+            const book = {
+                book: e.target.elements.book.value,
+                author: e.target.elements.author.value,
+                status: e.target.elements.status.value,
+            }
+            books.push(book);
+            localStorage.setItem('books', JSON.stringify(books));
+            console.log(books);
     })
 
 })
